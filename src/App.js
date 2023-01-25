0936af5b6df09c49f3afffb3a77604cb61efd6ca
Purushottam02 <purushottamgandas2002@gmail.com>
2023-01-25 21:12:26 +0530

    toggle buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.scss";
+import { useState } from "react";
 function App() {
+  const [on, setOn] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={on ? "switch on" : "switch"}onClick={()=>{
+      setOn(!on);
+    }}>
+      <div className={on ? "circle on" : "circle"} ></div>
     </div>
   );
 }
